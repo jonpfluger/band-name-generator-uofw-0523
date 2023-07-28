77f8baad7f367ab94615469c4394414c5b7ae345
jonpfluger <jonpfluger718@gmail.com>
2023-07-27 21:13:15 -0500

    chalk animation set up

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const {generateSlug} = require('random-word-slugs')
+import { generateSlug } from 'random-word-slugs'
+import chalkAnimation from 'chalk-animation'
 
 const numWords = parseInt(process.argv[2])
 
 const randomSlug = generateSlug(numWords, { format: 'title' })
-console.log(randomSlug)
+chalkAnimation.rainbow(randomSlug)
